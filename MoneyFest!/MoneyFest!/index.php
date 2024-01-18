41df1efdf31b1830c97e06188077bbb35e131c24
pelangisanrila <pelangisanrila@gmail.com>
2024-01-18 20:03:32 +0700

    MoneyFest! 3rd submission

--- MoneyFest!/MoneyFest!/index.php
+++ MoneyFest!/MoneyFest!/index.php
@@ -1,109 +1,129 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-   <head>
-      <!-- Required meta tags -->
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>MoneyFest! - Smart Financial Management</title>
-      <!-- Favicon -->
-      <link rel="shortcut icon" href="images/favicon.ico" />
-      <!-- Bootstrap CSS -->
-      <link rel="stylesheet" href="css/bootstrap.min.css">
-      <!-- Typography CSS -->
-      <link rel="stylesheet" href="css/typography.css">
-      <!-- Style CSS -->
-      <link rel="stylesheet" href="css/style.css">
-      <!-- Responsive CSS -->
-      <link rel="stylesheet" href="css/responsive.css">
-   </head>
-   <body>
-      <!-- loader Start -->
-      <div id="loading">
-         
-      <!-- <img class="img" src="img/polygon-2.svg" />
-<img class="polygon-2" src="img/polygon-4.svg" />
-<img class="polygon-3" src="img/polygon-3.svg" />
-<img class="polygon-4" src="img/polygon-5.svg" /> -->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    body {
+      background-image: #FFFFF3;
+    
+    }
+    .overlay {
+      position: absolute;
+      top: 50%;
+      left: 25%;
+      /* right: 70%; */
+      transform: translate(-50%, -50%);
+      /* text-align: center; */
+    }
+    .overlay h1 {
+      display: flex;
+      width: 573px;
+      height: 50px;
+      flex-direction: column;
+      justify-content: center;
+      flex-shrink: 0;
+      color: #5C5C5C;
+      font-family: Roboto;
+      font-size: 80px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 24px; /* 25.263% */
+      word-wrap: break-word;
+    }
+    .overlay h3 {
+      height: 100px; 
+      color: #5C5C5C; 
+      font-size: 20px; 
+      font-family: Roboto; 
+      font-weight: 400; 
+      line-height: 35px; 
+      word-wrap: break-word;
+    }
+    .overlay #button1 {
+      width: 120px; 
+      height: 50px;  
+      background-color: #F7F2FA;  
+      box-shadow: 0px 1px 3px 1px rgba(0, 0, 0, 0.15); 
+      border-radius: 10px; 
+      justify-content: center; 
+      align-items: center;
+      display: inline-flex;
+      text-align: center; 
+      color: #5C5C5C; 
+      font-size: 20px; 
+      font-family: Roboto; 
+      font-weight: 500; 
+      line-height: 20px; 
+      letter-spacing: 0.10px; 
+      word-wrap: break-word;
+      border-color: transparent;
+      text-decoration: none;
+    } 
+    .overlay #button2 {
+      left: 10px;
+      width: 120px; 
+      height: 50px; 
+      background-color: #5C5C5C; 
+      border-radius: 10px; 
+      justify-content: center; 
+      align-items: center; 
+      display: inline-flex;
+      text-align: center; 
+      color: white; 
+      font-size: 20px; 
+      font-family: Roboto; 
+      font-weight: 500; 
+      line-height: 20px; 
+      letter-spacing: 0.10px; 
+      word-wrap: break-word;
+      border-color: transparent;
+      text-decoration: none;
+    }
+    .overlay #tutorial {
+      position: fixed;
+      padding-top: 50%;
+    }
+    .overlay #tutorial iframe {
+      padding-left: 10%;
+    }
+    .overlay #tutorial h2 {
+      color: #5C5C5C;
+      font-family: Roboto;
+      font-size: 40px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 24px; /* 60% */
+      padding-bottom: 50px;
+    }
+    html {
+      scroll-behavior: smooth;
+    }
 
-      </div>
-      <!-- loader END -->
-        <!-- Sign in Start -->
-        <section class="sign-in-page">
-            <div class="container p-0">
-                <div class="row no-gutters">
-                    <div class="col-sm-12 align-self-center">
-                        <div class="sign-in-from bg-white">
-                            <h1 class="mb-0">Welcome</h1>
-                            <p>Enter your username and password</p>
-                            <form class="mt-4">
-                                <div class="form-group">
-                                    <label for="username">Username</label>
-                                    <input type="text" class="form-control mb-0" id="username" placeholder="Enter your username">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <a href="#" class="float-right">Forgot password?</a>
-                                    <input type="password" class="form-control mb-0" id="exampleInputPassword1" placeholder="Password">
-                                </div>
-                                <div class="form-group">
-                                    <label for="gender">Role</label>
-                                    <select class="form-control" id="gender" name="gender" placeholder="Your Full Name">
-                                        <option value="" disabled selected hidden>Choose your role</option>
-                                        <option value="male">User</option>
-                                        <option value="female">Financial Consultant</option>
-                                    </select>
-                                </div>
-                                <div class="d-inline-block w-100">
-                                    <div class="custom-control custom-checkbox d-inline-block mt-2 pt-1">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Remember Me</label>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary float-right">Login</button>
-                                </div>
-                                <div class="sign-info">
-                                    <span class="dark-color d-inline-block line-height-2">Don't have an account? <a href="sign-up.php">Sign up</a></span>
-                                    <ul class="iq-social-media">
-                                        <li><a href="#"><i class="ri-facebook-box-line"></i></a></li>
-                                        <li><a href="#"><i class="ri-twitter-line"></i></a></li>
-                                        <li><a href="#"><i class="ri-instagram-line"></i></a></li>
-                                    </ul>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Sign in END -->
-      <!-- Optional JavaScript -->
-      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.min.js"></script>
-      <!-- Appear JavaScript -->
-      <script src="js/jquery.appear.js"></script>
-      <!-- Countdown JavaScript -->
-      <script src="js/countdown.min.js"></script>
-      <!-- Counterup JavaScript -->
-      <script src="js/waypoints.min.js"></script>
-      <script src="js/jquery.counterup.min.js"></script>
-      <!-- Wow JavaScript -->
-      <script src="js/wow.min.js"></script>
-      <!-- Apexcharts JavaScript -->
-      <script src="js/apexcharts.js"></script>
-      <!-- Slick JavaScript -->
-      <script src="js/slick.min.js"></script>
-      <!-- Select2 JavaScript -->
-      <script src="js/select2.min.js"></script>
-      <!-- Owl Carousel JavaScript -->
-      <script src="js/owl.carousel.min.js"></script>
-      <!-- Magnific Popup JavaScript -->
-      <script src="js/jquery.magnific-popup.min.js"></script>
-      <!-- Smooth Scrollbar JavaScript -->
-      <script src="js/smooth-scrollbar.js"></script>
-      <!-- Chart Custom JavaScript -->
-      <script src="js/chart-custom.js"></script>
-      <!-- Custom JavaScript -->
-      <script src="js/custom.js"></script>
-   </body>
+  </style>
+</head>
+<body>
+  <div class="container">
+    <img src="images/Rectangle1.png" style="width: 750px; height: 1600px; left: -17px; top: -15px; position: absolute;" alt="">
+    <img src="images/cover.png" alt="" style="width: 750px; height: auto; padding-left: 505px;">
+    <img src="images/logo.png"  alt="" style="width: 100px; height: auto; left: 5px; top: -5px; position: absolute">
+  <div class="overlay">
+    <h1>MoneyFest!</h1>
+    <h3>An Integrated Platform for Financial Education <br>and Management Towards a Sustainable Future <br>Based on a Website.</h3>
+    <a href="#tutorial" id="button1">Learn More</a>&nbsp;
+    <a href="sign-in.php" id="button2">Sign in</a>
+    <div id="tutorial">
+      <h2>How To Use This Website ?</h2>
+      <iframe width="1000" height="515" src="https://www.youtube.com/embed/C9SqURRhriQ?si=bo1kFEoEkV3rMC8S" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</div>
+
+<script>
+  document.getElementById("button1").addEventListener("click", function() {
+    document.getElementById("tutorial").style.display = "block";
+  });
+</script>
+
+</body>
 </html>
